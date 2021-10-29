43fe789d6d64abe6acb4244bac07bae5d5f6e96d
NABurov <NABurov@greenatom.ru>
2021-10-29 23:19:18 +0300

    new recipeDB model; user class; recipes dataclass

--- app/conn.py
+++ app/conn.py
@@ -1,170 +1,141 @@
 import os
-from random import choice
+import random
 from typing import Optional
 from bson.objectid import ObjectId
 
 from pymongo import MongoClient
 from pymongo.collection import Collection
+from pydantic import BaseModel, Field
 
 
-class MongoRecipe:
+class Recipe(BaseModel):
+    id: Optional[ObjectId] = Field(default=None, alias='_id')
+    name: str
+    is_used: bool = False
+
+    def take(self):
+        if self.is_used:
+            raise RuntimeError(f'recipe {self} already used')
+        self.is_used = True
+        return self
+
+    def get_index(self):
+        return self.dict(include={'id'}, by_alias=True)
+
+    def get_data_without_index(self):
+        return self.dict(exclude={'id'}, by_alias=True)
+
+    class Config:
+        arbitrary_types_allowed = True
+
+
+class User:
+    @property
+    def name(self):
+        return self.__collection.name
+
+    def __init__(self, collection: Collection):
+        self.__collection = collection
+
+    def is_empty(self):
+        return not bool(self.list_recipes())
+
+    def list_recipes(self, filter: dict = {}) -> list[Recipe]:
+        """
+        :param filter: - фильтр по полям рецептов.
+
+        Example:
+        ```
+        user.list_recipes({'is_used': True})
+        >>> [Recipe(id=..., name=..., is_used=True)]
+        ```
+        """
+        cursor = self.__collection.find(filter)
+        return [Recipe(**document) for document in cursor]
+
+    def add_recipe(self, recipe_name: str):
+        new_recipe = Recipe(name=recipe_name)
+        self.__collection.insert_one(new_recipe.get_data_without_index())
+
+    def take_recipe(self, recipe: Recipe) -> Recipe:
+        recipe_index_filter = recipe.get_index()
+        used_recipe = recipe.take()
+        as_used_update = {'$set': used_recipe.get_data_without_index()}
+        self.__collection.update_one(filter=recipe_index_filter,
+                                     update=as_used_update)
+        return used_recipe
+
+    def remove_recipe(self, recipe: Recipe):
+        self.__collection.delete_one(filter=recipe.dict(by_alias=True))
+
+    def unuse_all_recipes(self):
+        as_unused_update = {'$set': {'is_used': False}}
+        self.__collection.update_many(filter={}, update=as_unused_update)
+
+    def random_recipe(self) -> Recipe:
+        if self.is_empty():
+            raise IndexError(f'User {self.name} has no recips')
+        unused_recipes = self.list_recipes(filter={'is_used': False})
+        if not unused_recipes:
+            self.unuse_all_recipes()
+            unused_recipes = self.list_recipes(filter={'is_used': False})
+        random_recipe = random.choice(unused_recipes)
+        return self.take_recipe(random_recipe)
+
+
+class RecipeDB:
     def __init__(self):
-        user = os.environ['MONGO_USER']
+        db_user = os.environ['MONGO_USER']
         password = os.environ['MONGO_PASSWORD']
         host = os.environ['MONGO_HOST']
         port = os.environ['MONGO_PORT']
-        db_name = os.environ['MONGO_DB']
+        db_name = os.environ['MONGO_RECIPE_DB']
 
-        connection_string = f'mongodb://{user}:{password}@{host}:{port}'
+        connection_string = f'mongodb://{db_user}:{password}@{host}:{port}'
         self.client = MongoClient(connection_string)
         self.db = self.client[db_name]
 
-    def __dispatch_user(self, id):
-        return f'user{id}'
+    def __get_user(self, user_id: int) -> User:
+        return User(self.db[user_id])
 
-    def __get_user_collection(self, user_id: int) -> Collection:
-        return self.db[self.__dispatch_user(user_id)]
+    def add_recipe(self, user_id: int, recipe_name: str):
+        user = self.__get_user(user_id)
+        user.add_recipe(recipe_name)
 
-    def insert_recipe(self, user_id: int, recipe: str) -> None:
-        if not isinstance(recipe, str):
-            raise TypeError('recipe must be a string')
+    def list_recipes(self, user_id: int) -> list[Recipe]:
+        user = self.__get_user(user_id)
+        return user.list_recipes()
 
-        new_recipe = {
-            'name': recipe,
-            'details': '',
-            'used': False,
-        }
-        user_collection = self.__get_user_collection(user_id)
-        user_collection.insert_one(new_recipe)
+    def remove_recipe(self, user_id: int, recipe: Recipe):
+        user = self.__get_user(user_id)
+        user.remove_recipe(recipe)
 
-    def list_user(self) -> list:
-        return self.db.list_collection_names()
-
-    def list_recipe(self, user_id: int) -> list:
-        user_collection = self.__get_user_collection(user_id)
-        projection = {
-            '_id': False,
-            'details': False,
-        }
-        cursor = user_collection.find(projection=projection)
-        return [doc for doc in cursor]
-
-    def list_unused_recipe_id(self, user_id: int) -> list:
-        user_collection = self.__get_user_collection(user_id)
-        filter = {
-            'used': False,
-        }
-        projection = {
-            '_id': True
-        }
-        cursor = user_collection.find(filter=filter, projection=projection)
-        return [doc['_id'] for doc in cursor]
-
-    def take_recipe(self, user_id: int, object_id: ObjectId) -> Optional[str]:
-        user_collection = self.__get_user_collection(user_id)
-        filter = {
-            '_id': object_id,
-            'used': False,
-        }
-        update = {
-            '$set': {'used': True}
-        }
-        document = user_collection.find_one_and_update(filter=filter,
-                                                       update=update)
-        try:
-            recipe = document['name']
-        except TypeError:
-            recipe = None
-
-        return recipe
-
-    def is_empty_user(self, user_id: int) -> bool:
-        user_collection = self.__get_user_collection(user_id)
-        exist_doc = user_collection.find_one()
-        return not bool(exist_doc)
-
-    def tag_all_as_unused(self, user_id: int) -> None:
-        user_collection = self.__get_user_collection(user_id)
-        filter = {
-            'used': True
-        }
-        update = {
-            '$set': {'used': False}
-        }
-        user_collection.update_many(filter=filter, update=update)
-
-    def random_recipe(self, user_id: int) -> str:
-        if self.is_empty_user(user_id):
-            raise IndexError('There is no recipes')
-        recipes = self.list_unused_recipe_id(user_id)
-        if recipes:
-            random_recipe = self.take_recipe(user_id, choice(recipes))
-        else:
-            self.tag_all_as_unused(user_id)
-            random_recipe = self.random_recipe(user_id)
-        return random_recipe
-
-    def change_details(self, user_id: int, recipe: str, details: str) -> None:
-        user_collection = self.__get_user_collection(user_id)
-        filter = {
-            'name': recipe,
-        }
-        update = {
-            '$set': {'details': details}
-        }
-        user_collection.update_one(filter=filter, update=update)
-
-    def change_recipe_name(
-        self, user_id: int, recipe: str, new_name: str
-    ) -> None:
-        user_collection = self.__get_user_collection(user_id)
-        filter = {
-            'name': recipe,
-        }
-        update = {
-            '$set': {'name': new_name}
-        }
-        user_collection.update_one(filter=filter, update=update)
-
-    def get_recipe_details(self, user_id: int, recipe: str) -> str:
-        user_collection = self.__get_user_collection(user_id)
-        filter = {
-            'name': recipe,
-        }
-        projection = {
-            '_id': False,
-            'details': True,
-        }
-        document = user_collection.find_one(filter=filter,
-                                            projection=projection)
-        if document:
-            details = document['details']
-        else:
-            details = None
-        return details
-
-    def delete_recipe(self, user_id: int, recipe: str) -> None:
-        user_collection = self.__get_user_collection(user_id)
-        filter = {
-            'name': recipe,
-        }
-        user_collection.delete_one(filter=filter)
+    def random_recipe(self, user_id: int) -> Recipe:
+        user = self.__get_user(user_id)
+        return user.random_recipe()
 
 
 if __name__ == '__main__':
-    db = MongoRecipe()
-    # print(db.list_user())
-    # print(db.create_user(15))
-    print(db.list_user())
-    # print(db.list_recipe(1))
-    # print(db.insert_recipe(13, 'kartoshka'))
-    # print(db.list_recipe(13))
-    # unused_rec = db.list_unused_recipe_id(13)
-    # print(unused_rec)
-    # print(db.take_recipe(13, choice(unused_rec)))
-    # print(db.list_unused_recipe_id(13))
-    print(db.list_recipe(272502933))
-    print(db.is_empty_user(272502933))
-    u_r = db.list_unused_recipe_id(272502933)
-    print(db.take_recipe(272502933, choice(u_r)))
-    # print(db.take_recipe(272502933, ObjectId('60d7039b7732911c74cf7dfe')))
+    db = RecipeDB()
+    test_user_id = '42'
+
+    recipes = db.list_recipes(test_user_id)
+    assert recipes == []
+
+    test_recipe = {'name': 'kukuha', 'is_used': False}
+    db.add_recipe(test_user_id, test_recipe['name'])
+    recipes = db.list_recipes(test_user_id)
+    assert recipes[0].get_data_without_index() == test_recipe
+
+    test_recipe['is_used'] = True
+    random_recipe = db.random_recipe(test_user_id)
+    assert random_recipe.get_data_without_index() == test_recipe
+
+    random_recipe = db.random_recipe(test_user_id)
+    assert random_recipe.get_data_without_index() == test_recipe
+
+    recipes = db.list_recipes(test_user_id)
+    for r in recipes:
+        db.remove_recipe(test_user_id, r)
+    recipes = db.list_recipes(test_user_id)
+    assert recipes == []
