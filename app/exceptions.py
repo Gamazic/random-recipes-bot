f4e423ee51ea74d72e0d35799bec9edbaedacd3c
NABurov <NABurov@greenatom.ru>
2022-01-12 23:05:24 +0300

    user has no selected recipe exception

--- app/exceptions.py
+++ app/exceptions.py
@@ -4,3 +4,8 @@
 class UserHasNoRecipesError(Exception):
     """В базе данных отсутсвуют рецепты для пользователя"""
     pass
+
+
+class UserHasNoSelectedRecipeError(Exception):
+    """В базе данных отсутсвует конкретный рецепт для пользователя"""
+    pass
