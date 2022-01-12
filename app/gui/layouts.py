6aa8deabf27825f7f1a4a7b992b460c39bea5d03
NABurov <NABurov@greenatom.ru>
2022-01-12 23:06:36 +0300

    deleted list of recipes layout method and moved to server func

--- app/gui/layouts.py
+++ app/gui/layouts.py
@@ -9,15 +9,3 @@ def create_recipe_details_layout(recipe: RecipeWithId) -> dict:
                    f'*{recipe.name}*\n' \
                    f'Статус: *{is_used_status_line}*'
     return dict(text=details_text, reply_markup=markup)
-
-
-def create_recipes_list_layout(chat_id: int, db: Database) -> dict:
-    # TODO: Можно избавится от этого метода и в колбеке вызывать show_recipes_list.
-    # Для этого нужно удалять предыдущий layout с recipe_details.
-    recipes = db.list_user_recipes(chat_id)
-    if not recipes:
-        return dict(text='Список рецептов пуст.')
-    else:
-        markup = recipes_list_inline_keyboard_markup(recipes)
-        text = 'Список рецептов:'
-        return dict(text=text, reply_markup=markup)
